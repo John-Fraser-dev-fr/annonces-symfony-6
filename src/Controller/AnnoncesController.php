94580ba967a97f2ca61fe9c398d011cd93a1a4dc
John Fraser <burton_fsr@icloud.com>
2022-03-11 15:02:21 +0100

    show annonce

--- src/Controller/AnnoncesController.php
+++ src/Controller/AnnoncesController.php
@@ -11,15 +11,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class AnnoncesController extends AbstractController
 {
     #[Route('/index', name: 'app_annonces')]
-    public function index(AnnonceRepository $repo, ImageRepository $repo2): Response
+    public function index(AnnonceRepository $repo): Response
     {
-
         $annonces = $repo->findAll();
-
 
         return $this->render('annonces/index.html.twig', [
             'controller_name' => 'AnnoncesController',
             'annonces'=> $annonces,
+        ]);
+    }
+
+
+    #[Route('/{id}', name: 'show_annonce')]
+    public function show(AnnonceRepository $repo, $id, ImageRepository $repo2)
+    {
+        $annonce = $repo->find($id); 
+
+        $images = $repo2->findBy(['annonce'=>$id]); 
+
+        return $this->render('annonces/show.html.twig',[
+            'annonce' => $annonce,
+            'images' => $images,
         ]);
     }
 }
