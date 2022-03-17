e344ad2894e5bd90b0cea9e8e4d25d5917a05a96
John Fraser <burton_fsr@icloud.com>
2022-03-17 11:25:20 +0100

    Formulaire ajout annonce

--- src/Controller/AnnoncesController.php
+++ src/Controller/AnnoncesController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonce;
+use App\Form\AnnonceType;
 use App\Repository\AnnonceRepository;
 use App\Repository\ImageRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +19,7 @@ class AnnoncesController extends AbstractController
     #[Route('/index', name: 'app_annonces')]
     public function index(AnnonceRepository $repo): Response
     {
+        //Récupére toutes les annonces
         $annonces = $repo->findAll();
 
         return $this->render('annonces/index.html.twig', [
@@ -22,17 +29,48 @@ class AnnoncesController extends AbstractController
 
 
     #[Route('/add', name: 'add_annonce')]
-    public function add(): Response
+    public function add(Request $request, EntityManagerInterface $entityManager, UserRepository $user): Response
     {   
-        return $this->render('annonces/add.html.twig');
+        //Création d'un nouvel objet Annonce
+        $annonce = new Annonce();
+
+        //Formulaire relié à l'entité Annonce
+        $formAnnonce = $this->createForm(AnnonceType::class, $annonce);
+
+        //Analyse de la requete
+        $formAnnonce->handleRequest($request);
+
+        if($formAnnonce->isSubmitted() && $formAnnonce->isValid())
+        {
+
+            //Récupération de l'id user
+            $user = $this->getUser();
+
+            $annonce->setDate(new \DateTime())
+                    ->setUser($user);
+           
+
+            //Enregistrement en BDD
+            $entityManager->persist($annonce);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_annonces');
+        }
+
+
+        return $this->render('annonces/add.html.twig',[
+            'formAnnonce'=> $formAnnonce->createView()
+        ]);
     }
 
 
     #[Route('/annonce/{id}', name: 'show_annonce')]
     public function show(AnnonceRepository $repo, $id, ImageRepository $repo2)
     {
+        //Récupére l'annonce grace à l'ID (GET)
         $annonce = $repo->find($id); 
 
+        //Récupére les images correspondantes à l'ID de l'annonce
         $images = $repo2->findBy(['annonce'=>$id]); 
 
         return $this->render('annonces/show.html.twig',[
---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonce;
+use Doctrine\DBAL\Types\FloatType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnonceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('nb_pieces', IntegerType::class)
+            ->add('surface', IntegerType::class)
+            ->add('prix', IntegerType::class)
+            ->add('cd_postal', IntegerType::class)
+            ->add('ville', TextType::class)
+            ->add('type', TextType::class)
+            ->add('imageCover', UrlType::class)
+            ->add('Ajouter', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonce::class,
+        ]);
+    }
+}
